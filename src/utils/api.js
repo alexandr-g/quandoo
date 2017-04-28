ebd601811bcaf7de530e16a26e8bf09e605c4b1c
Alexandr <alexgrischuk@gmail.com>
2017-04-28 22:54:37 +0200

    Get user from GitHub by username api helper

---
+++ src/utils/api.js
@@ -0,0 +1,10 @@
+let api = {
+  getBio(username) {
+    username = username.toLowerCase().trim()
+    let url = `https://api.github.com/users/${username}`
+    
+    return fetch(url).then((res) => res.json())
+  }
+}
+
+export default api
