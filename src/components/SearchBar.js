f07fb6a343a1f95e78fc34d274fbb0bfb937df45
Alexandr <alexgrischuk@gmail.com>
2017-04-29 21:55:05 +0200

    Grab user input and put it in state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,37 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      username: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange (event) {
+    this.setState({username: event.target.value})
+  }
+
+  handleSubmit (event) {
+    console.log('A name was submitted: ' + this.state.username)
+    event.preventDefault()
+  }
+
   render () {
+    console.log(this.state)
     return (
-      <form>
-        <div className='form-group row'>
-          <div className='container-fluid'>
-            <input id='search-input' type='search' className='form-control' placeholder='Search for GitHub user' />
-          </div>
+      <form onSubmit={this.handleSubmit}>
+        <div className='container-fluid'>
+          <input
+            id='search-input'
+            type='text'
+            className='form-control'
+            placeholder='Search for GitHub user'
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
         </div>
       </form>
     )
