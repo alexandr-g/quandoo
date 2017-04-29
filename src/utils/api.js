859785917d5687ae82036f8bb34118b393bbb335
Alexandr <alexgrischuk@gmail.com>
2017-04-29 22:37:43 +0200

    Get github repos

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,12 @@ let api = {
     let url = `https://api.github.com/users/${username}`
 
     return fetch(url).then((res) => res.json())
+  },
+  getRepos (username) {
+    username = username.toLowerCase().trim()
+    let url = `https://api.github.com/users/${username}/repos`
+
+    return fetch(url).then((res) => res.json())
   }
 }
 
