fe7dd243c19d96623e72378074fa872594aa572d
Alexandr <alexgrischuk@gmail.com>
2017-04-30 11:34:51 +0200

    Put Results component tests into test suite

--- __tests__/Results.spec.js
+++ __tests__/Results.spec.js
@@ -1,20 +1,24 @@
+/* global describe, test, expect */
+
 import React from 'react'
 import Results from '../src/components/Results'
 import renderer from 'react-test-renderer'
 
-test('results list is rendered without user info object', () => {
-  const component = renderer.create(<Results />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-})
+describe('Results component', () => {
+  test('results list is rendered without user info object', () => {
+    const component = renderer.create(<Results />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 
-test('results list is rendered with user info object', () => {
-  const userInfo = {
-    name: 'test',
-    login: 'test login',
-    location: 'planet earth'
-  }
-  const component = renderer.create(<Results userInfo={userInfo} />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+  test('results list is rendered with user info object', () => {
+    const userInfo = {
+      name: 'test',
+      login: 'test login',
+      location: 'planet earth'
+    }
+    const component = renderer.create(<Results userInfo={userInfo} />)
+    const tree = component.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
 })
