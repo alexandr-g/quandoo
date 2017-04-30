855298477931452454f275664ee25fe0b2c5b108
Alexandr <alexgrischuk@gmail.com>
2017-04-30 11:30:47 +0200

    Add App test to assert h1 is rendered with a correct value

--- __tests__/App.spec.js
+++ __tests__/App.spec.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
 import App from '../src/components/App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<App />, div)
+  })
+
+  it('renders h1 inside App with a Quandoo value', () => {
+    const component = shallow(<App />)
+    expect(component.find('h1').text()).toEqual('Quandoo')
+  })
 })
