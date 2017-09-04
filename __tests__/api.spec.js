f0f4a975133c4300c91f24534bb25047f880f4f7
Oleksandr Hrishchuk <alexgrischuk@gmail.com>
2017-09-04 11:42:41 +0200

    Format

--- __tests__/api.spec.js
+++ __tests__/api.spec.js
@@ -1,23 +1,24 @@
 /* global jest, describe, it, expect */
-import "isomorphic-fetch";
+import 'isomorphic-fetch'
 
-jest.mock("./__mocks__/request");
-const api = require("../src/utils/api");
+jest.mock('./__mocks__/request')
+const api = require('../src/utils/api')
 
-describe("#getBio() using Promises", () => {
-  it.skip("should load user data and verify user name", () => {
-    return api.getBio("alexandr-g").then(data => {
-      expect(data).toBeDefined();
-      expect(data.name).toEqual("Alexander Grischuk");
-    });
-  });
-});
+describe('#getBio() using Promises', () => {
+  it.skip('should load user data and verify user name', () => {
+    return api.getBio('alexandr-g')
+    .then(data => {
+      expect(data).toBeDefined()
+      expect(data.name).toEqual('Alexander Grischuk')
+    })
+  })
+})
 
 // using async/await
-describe("#getBio() using async/await", () => {
-  it.skip("should load user data and verify login value", async () => {
-    const data = await api.getBio("alexandr-g");
-    expect(data).toBeDefined();
-    expect(data.login).toEqual("alexandr-g");
-  });
-});
+describe('#getBio() using async/await', () => {
+  it.skip('should load user data and verify login value', async () => {
+    const data = await api.getBio('alexandr-g')
+    expect(data).toBeDefined()
+    expect(data.login).toEqual('alexandr-g')
+  })
+})
