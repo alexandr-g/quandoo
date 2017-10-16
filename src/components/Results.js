2f040960068bd113b3528f1f27ac31f6aa32c7f7
Alexander Grischuk <alexgrischuk@gmail.com>
2017-10-16 22:04:02 +0200

    Style results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import MyErrorBoundary from './MyErrorBoundary'
 
 const Results = ({ userInfo }) => {
@@ -11,16 +12,16 @@ const Results = ({ userInfo }) => {
         <div className='card-block'>
           <ul className='list-group'>
             <li className='list-group-item'>
-              <b>Name:</b> {name}
+              <b>Name:</b> <TextTile> {name} </TextTile>
             </li>
             <li className='list-group-item'>
-              <b>Login:</b> {login}
+              <b>Login:</b> <TextTile> {login} </TextTile>
             </li>
             <li className='list-group-item'>
-              <b>Location:</b> {location}
+              <b>Location:</b> <TextTile> {location} </TextTile>
             </li>
             <li className='list-group-item'>
-              <b>Bio:</b> {bio}
+              <b>Bio:</b> <TextTile> {bio} </TextTile>
             </li>
           </ul>
         </div>
@@ -32,5 +33,9 @@ const Results = ({ userInfo }) => {
 Results.propTypes = {
   userInfo: PropTypes.object
 }
+
+const TextTile = styled.span`
+  padding-left: 7px
+`
 
 export default Results
