ce71ed1926451b530375ec3c6d90e248d965e05e
Alexander Grischuk <alexgrischuk@gmail.com>
2017-10-16 22:32:53 +0200

    Render Multiple Elements

---
+++ src/components/Fruits.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Fruits = () => [
+  <li key='1'>Apple</li>,
+  <li key='2'>Orange</li>,
+  <li key='3'>Banana</li>
+]
+
+export default Fruits
+
+// Render Multiple Elements
+
+/*
+  <div>
+    <ul>
+      <Fruits />
+      <li key='4'>Peach</li>
+      <li key='5'>Ananas</li>
+    </ul>
+  </div>
+ */
