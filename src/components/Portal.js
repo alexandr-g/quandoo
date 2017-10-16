42796799a78811b1a483920bbc54fe66073a1bfd
Alexander Grischuk <alexgrischuk@gmail.com>
2017-10-16 22:57:07 +0200

    Render Elements Outside the Current React Tree using Portals

---
+++ src/components/Portal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Portal = () => (
+  <div>
+    <h1>Dashboard</h1>
+    {ReactDOM.createPortal(
+      <div>Welcome</div>,
+      document.getElementById('portal-container')
+    )}
+  </div>
+)
+
+ReactDOM.render(<Portal />, document.getElementById('portal-container'))
