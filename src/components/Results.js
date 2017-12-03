1544496c481b431e137d53ab003464be54acc987
Alexander Grischuk <alexgrischuk@gmail.com>
2017-12-03 16:10:42 +0100

    Remove error boundary from results as it is handled on upper level

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,32 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import MyErrorBoundary from './MyErrorBoundary'
 
 const Results = ({ userInfo }) => {
   const { name, login, location, bio } = userInfo
 
   return (
-    <MyErrorBoundary>
-      <div className='container'>
-        <div className='card-block'>
-          <ul className='list-group'>
-            <li className='list-group-item'>
-              <b>Name:</b> <TextTile> {name} </TextTile>
-            </li>
-            <li className='list-group-item'>
-              <b>Login:</b> <TextTile> {login} </TextTile>
-            </li>
-            <li className='list-group-item'>
-              <b>Location:</b> <TextTile> {location} </TextTile>
-            </li>
-            <li className='list-group-item'>
-              <b>Bio:</b> <TextTile> {bio} </TextTile>
-            </li>
-          </ul>
-        </div>
+    <div className='container'>
+      <div className='card-block'>
+        <ul className='list-group'>
+          <li className='list-group-item'>
+            <b>Name:</b> <TextTile> {name} </TextTile>
+          </li>
+          <li className='list-group-item'>
+            <b>Login:</b> <TextTile> {login} </TextTile>
+          </li>
+          <li className='list-group-item'>
+            <b>Location:</b> <TextTile> {location} </TextTile>
+          </li>
+          <li className='list-group-item'>
+            <b>Bio:</b> <TextTile> {bio} </TextTile>
+          </li>
+        </ul>
       </div>
-    </MyErrorBoundary>
+    </div>
   )
 }
 
